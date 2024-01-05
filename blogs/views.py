3852f5fbf13f7a88dd343915060e2dd876916718
javvadigithub <j_srikanth@outlook.com>
2024-01-05 12:04:55 +0530

    Login & Registration

--- blogs/views.py
+++ blogs/views.py
@@ -29,5 +29,6 @@ def search(request):
     blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status='Published')
     context = {
         'blogs': blogs,
+        'keyword': keyword,
     }
     return render(request, 'search.html')
